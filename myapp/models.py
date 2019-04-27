9e6f3691d54364e2f19b71fae068ddb03c7b4efe
e6440ulinux <natanbarbosa15@gmail.com>
2019-04-27 18:23:49 -0300

    Template

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
-class login(models.Model):
-    email = models.EmailField;
+class Login(models.Model):
+    email = models.EmailField(max_length=50, default="null")
+    password = models.CharField(max_length=16, default="null")
+    dataCriacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
+
+    data = {}
+
+    data['now'] = datetime.datetime.now()
     # html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'myapp/home.html')
+    return render(request, 'myapp/home.html', data)
